5e4ecd122047ba367994b51c308b1b3416a8589d
Joseph Semrai <josephsemrai@gmail.com>
2020-03-26 20:05:37 -0400

    Finish multi-course pathway setup

--- src/components/SetupUtility/SelectionStep/MultiPathwaySetup.js
+++ src/components/SetupUtility/SelectionStep/MultiPathwaySetup.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import PathwayCreate from 'src/views/PathwayCreate';
 import ModuleCreate from 'src/views/ModuleCreate';
+import { Typography, CircularProgress } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { joinPathway } from 'src/actions/pathwayActions';
 
 const ExistingPathwaySetup = () => {
   const [pathwayId, setPathwayId] = useState();
   const [currentStep, setCurrentStep] = useState('createPathway');
+  const dispatch = useDispatch();
 
   const goToModuleStep = id => {
     setPathwayId(id);
     setCurrentStep('createModule');
+    // eslint-disable-next-line no-alert
     alert(
       'Now, create a module to hold your courses. This module will automatically be added to the pathway you just created.'
+    );
+  };
+
+  const goToFinalStep = () => {
+    setCurrentStep('joiningPathway');
+    dispatch(
+      joinPathway(
+        pathwayId,
+        () => alert('There was an error joining the pathway!'),
+        () => setCurrentStep('finished')
+      )
     );
   };
 
@@ -23,11 +39,37 @@ const ExistingPathwaySetup = () => {
           userFlowMode
         />
       );
-    } else if (currentStep === 'createModule') {
-      return <ModuleCreate className="createModule" userFlowMode />;
+    }
+    if (currentStep === 'createModule') {
+      return (
+        <ModuleCreate
+          className="createModule"
+          userFlowMode
+          userFlowPathwayId={pathwayId}
+          goToFinalStep={goToFinalStep}
+        />
+      );
     }
 
-    return <p>There was a problem rendering this component.</p>;
+    if (currentStep === 'joiningPathway') {
+      return (
+        <>
+          <CircularProgress />
+          <Typography>Joining Pathway...</Typography>
+        </>
+      );
+    }
+
+    if (currentStep === 'finished') {
+      return (
+        <Typography>
+          Successfully joined pathway. You&apos;re all set!
+        </Typography>
+      );
+    }
+    return (
+      <Typography>There was a problem rendering this component.</Typography>
+    );
   };
 
   return generateStep();
