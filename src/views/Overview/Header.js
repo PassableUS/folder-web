f24e9aae4b060acf48811cd80a18ffd7955ea16b
Joseph Semrai <josephsemrai@gmail.com>
2020-03-28 18:29:26 -0400

    Fix view today's plan button

--- src/views/Overview/Header.js
+++ src/views/Overview/Header.js
@@ -3,15 +3,11 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import {
-  Typography,
-  Grid,
-  Button,
-  Hidden
-} from '@material-ui/core';
+import { Typography, Grid, Button, Hidden } from '@material-ui/core';
 import BarChartIcon from '@material-ui/icons/BarChart';
+import { useHistory } from 'react-router';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {},
   summaryButton: {
     backgroundColor: theme.palette.common.white
@@ -27,61 +23,34 @@ const useStyles = makeStyles((theme) => ({
 
 function Header({ className, ...rest }) {
   const classes = useStyles();
-  const session = useSelector((state) => state.session);
+  const session = useSelector(state => state.session);
+  const history = useHistory();
 
   return (
-    <div
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <Grid
-        alignItems="center"
-        container
-        justify="space-between"
-        spacing={3}
-      >
-        <Grid
-          item
-          md={6}
-          xs={12}
-        >
-          <Typography
-            component="h2"
-            gutterBottom
-            variant="overline"
-          >
+    <div {...rest} className={clsx(classes.root, className)}>
+      <Grid alignItems="center" container justify="space-between" spacing={3}>
+        <Grid item md={6} xs={12}>
+          <Typography component="h2" gutterBottom variant="overline">
             Home
           </Typography>
-          <Typography
-            component="h1"
-            gutterBottom
-            variant="h3"
-          >
-            Hey
-            {' '}
-            {session.user.firstName}
-            !
+          <Typography component="h1" gutterBottom variant="h3">
+            Hey {session.user.firstName}!
           </Typography>
-          <Typography
-            gutterBottom
-            variant="subtitle1"
-          >
+          <Typography gutterBottom variant="subtitle1">
             We've prepared your lesson plan for today.
           </Typography>
           <Button
             className={classes.summaryButton}
             edge="start"
             variant="contained"
+            onClick={() => history.push('/calendar')}
           >
             <BarChartIcon className={classes.barChartIcon} />
             View today's plan
           </Button>
         </Grid>
         <Hidden smDown>
-          <Grid
-            item
-            md={6}
-          >
+          <Grid item md={6}>
             <img
               alt="Cover"
               className={classes.image}
